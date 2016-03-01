32f0bb02403c5e2d406b4544bb0ccc54ec720820
Tim Huisman <tim@hoppinger.com>
2016-03-01 17:31:19 +0100

    Add Gulp for asset compiling

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss    = require('gulp-minify-css'),
+    rename       = require('gulp-rename'),
+    clean        = require('gulp-clean');
+
+var src  = 'src/stylesheets/csmb.scss',
+    dist = 'dist/stylesheets';
+
+// Clean
+gulp.task('clean-styles', function () {
+  gulp.src(dist, {read: false})
+    .pipe(clean());
+});
+
+// Styles
+gulp.task('styles', function() {
+  gulp.src(src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(dist))
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 4 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(dist))
+    .pipe(rename({ suffix: '-min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest(dist));
+});
+
+// Default
+gulp.task('default', ['clean-styles', 'styles']);
+
+// Watch
+gulp.task('watch', function(callback) {
+  gulp.watch(src, ['styles']);
+});
